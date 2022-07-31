4772ac2606faf6db8a38c33fd1aa3c6ad910602b
Lukma Gayo Arizky <lukma.gayo.arizky@gmail.com>
2022-07-31 10:36:06 +0700

    [Android] Prepare home screen

---
+++ androidApp/src/main/java/com/gplay/app/feature/home/HomeView.kt
@@ -0,0 +1,17 @@
+package com.gplay.app.feature.home
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import com.gplay.app.ui.theme.GPlayTheme
+
+@Composable
+fun HomeView() {
+}
+
+@Preview(showBackground = true)
+@Composable
+private fun DefaultPreview() {
+    GPlayTheme {
+        HomeView()
+    }
+}
--- androidApp/src/main/java/com/gplay/app/ui/GPlayApp.kt
+++ androidApp/src/main/java/com/gplay/app/ui/GPlayApp.kt
@@ -4,12 +4,16 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.rememberNavController
+import com.gplay.android.navigation.build
+import com.gplay.app.feature.home.HomeRoute
+import com.gplay.app.feature.home.HomeScreen
 import com.gplay.app.ui.theme.GPlayTheme
 
 @Composable
 fun GPlayApp() {
     val navController = rememberNavController()
-    NavHost(navController = navController, startDestination = "home") {
+    NavHost(navController = navController, startDestination = HomeRoute.route) {
+        HomeScreen.build(this)
     }
 }
 
