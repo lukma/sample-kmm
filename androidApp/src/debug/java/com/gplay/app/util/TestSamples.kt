af027680639575541b9377a8c76ae5e3da47f9b4
Lukma Gayo Arizky <lukma.gayo.arizky@gmail.com>
2022-08-02 05:15:26 +0700

    [Android] Create test samples in android app module

---
+++ androidApp/src/debug/java/com/gplay/app/util/TestSamples.kt
@@ -0,0 +1,10 @@
+package com.gplay.app.util
+
+object TestSamples {
+    // Common
+    const val errorMessage = "some error"
+
+    // Auth
+    const val username = "john.doe@mail.com"
+    const val password = "qwerty"
+}
