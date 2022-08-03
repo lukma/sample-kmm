e98855f4991b16e27831e36084adef9cd0d33903
Lukma Gayo Arizky <lukma.gayo.arizky@gmail.com>
2022-08-03 22:59:22 +0700

    [Multiplatform] Make ValidationState nullable in ValidationStates

--- shared/common/src/commonMain/kotlin/com/gplay/core/domain/validation/ValidationState.kt
+++ shared/common/src/commonMain/kotlin/com/gplay/core/domain/validation/ValidationState.kt
@@ -5,6 +5,17 @@ sealed class ValidationState {
     data class Invalid(val error: ValidationError) : ValidationState()
 }
 
-typealias ValidationStates = Map<FieldSpec, ValidationState>
+typealias ValidationStates = Map<FieldSpec, ValidationState?>
 
 inline val ValidationState.isInValid get() = this is ValidationState.Invalid
+
+fun ValidationStates.isAllValid(): Boolean {
+    if (isEmpty()) return false
+
+    forEach { (_, value) ->
+        if (value == null || value.isInValid) {
+            return false
+        }
+    }
+    return true
+}
