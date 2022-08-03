5cd70c6b64d68b317ae33235b8a97685253d8f2e
Lukma Gayo Arizky <lukma.gayo.arizky@gmail.com>
2022-08-04 01:49:57 +0700

    [Android] Create validation error message extension

---
+++ androidApp/src/main/java/com/gplay/app/util/validation/ValidationErrorExt.kt
@@ -0,0 +1,18 @@
+package com.gplay.app.util.validation
+
+import android.content.Context
+import com.gplay.app.R
+import com.gplay.core.domain.validation.ValidationError
+
+fun ValidationError.message(context: Context, vararg formatArgs: Any): String {
+    val resourceId = when (this) {
+        is ValidationError.FieldBlank -> R.string.error_textfield_blank
+    }
+
+    val text = if (formatArgs.isEmpty()) {
+        context.getString(resourceId)
+    } else {
+        context.getString(resourceId, *formatArgs)
+    }
+    return text
+}
