f9fad1886256395072038269b1fc939dffa2a54c
Lukma Gayo Arizky <lukma.gayo.arizky@gmail.com>
2022-08-17 18:33:47 +0700

    [Apple] Add xcode project

---
+++ appleApp/Shared/GPlayApp.swift
@@ -0,0 +1,17 @@
+//
+//  GPlayApp.swift
+//  Shared
+//
+//  Created by Lukma Gayo Arizky on 17/08/22.
+//
+
+import SwiftUI
+
+@main
+struct GPlayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
