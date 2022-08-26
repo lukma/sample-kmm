541caa571e7e09c07df53e4fb98807683ba6d054
Lukma Gayo Arizky <lukma.gayo.arizky@gmail.com>
2022-08-27 03:27:02 +0700

    [Multiplatform] Add extensions to access result in apple platform

--- shared/common/src/commonMain/kotlin/com/gplay/core/domain/common/entity/Result.kt
+++ shared/common/src/commonMain/kotlin/com/gplay/core/domain/common/entity/Result.kt
@@ -5,14 +5,20 @@ sealed class Result<out V> {
     data class Failure(val error: Throwable) : Result<Nothing>()
 }
 
+fun <V> Result<V>.isSuccess() = this is Result.Success
+
 fun <V> Result<V>.onSuccess(block: (V) -> Unit) = apply {
     if (this is Result.Success) {
-        block(this.value)
+        block(value)
     }
 }
 
 fun <V> Result<V>.onFailure(block: (Throwable) -> Unit) = apply {
     if (this is Result.Failure) {
-        block(this.error)
+        block(error)
     }
+}
+
+fun <V> Result<V>.exceptionOrNull(): Throwable? {
+    return if (this is Result.Failure) error else null
 }
